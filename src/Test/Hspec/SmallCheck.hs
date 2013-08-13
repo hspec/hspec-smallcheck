bf8742de788d9482ee94c0d2647b5c9efdf5b152
Simon Hengel <sol@typeful.net>
2013-08-13 23:08:13 +0200

    Add example

--- src/Test/Hspec/SmallCheck.hs
+++ src/Test/Hspec/SmallCheck.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-module Test.Hspec.SmallCheck where
+module Test.Hspec.SmallCheck (property) where
 
 import           Control.Applicative
 import           Test.Hspec.Core
 import           Test.SmallCheck
 import           Test.SmallCheck.Drivers
+
+property :: Testable IO a => a -> Property IO
+property = test
 
 instance Example (Property IO) where
   evaluateExample c p = maybe Success (Fail . ppFailure) <$> smallCheckM (paramsSmallCheckDepth c) p
